83d457ca6374a357d462d336c7d14726a65408e0
lohitdeva <devaboi8@gmail.com>
2022-06-03 00:14:49 +0530

    Some more beautification changes

--- index.php
+++ index.php
@@ -3,6 +3,21 @@
 
     <head>
 
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <!-- FontAwesome CSS -->
+        <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Tiro+Telugu&display=swap" rel="stylesheet">
+
+
         <style>
 
             #logo {
@@ -34,35 +49,47 @@
             }
 
             .icon {
+                color: white;
                 padding: 10px;
-                background-color: #b331ce;
+                background-color: #7A896B;
                 border-radius: .25rem;
-                border: 1px solid #ced4da;
+                border: 1px solid #15502C;
                 min-width: 50px;
                 text-align: center;
             }
 
             #form-box {
-                background-color: #a2171b;
+                background-color: #9C231B;
+            }
+
+            .btn-primary {
+                background-color: #15502C;
+                border-color: #0f3d20;
+            }
+
+            .btn-primary:hover {
+                background-color: #289b54;
+                border-color: #0f3d20;
+            }
+
+            .form-control {
+                border: 1px solid black;
+            }
+
+            .form-text {
+                font-family: 'Tiro Telugu', serif;
+                color: white;
+                font-size: 20px;
+                font-weight: bold;
             }
 
 
         </style>
 
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-        <!-- FontAwesome CSS -->
-        <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
         <title>Login</title> 
     </head>
 
-    <body style="background-color:#f6dbbd">
+    <body style="background-color:#ECDBAB">
 
         <div class="container">
 
@@ -75,7 +102,7 @@
             <div class="jumbotron" id='form-box'>
                 <form>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
+                        <label for="exampleInputEmail1" class='form-text'>Email address</label>
                         <div class="input-container">
                             <i class="fa fa-envelope icon"></i>
                             <input type="email" class="form-control" id="emailID" aria-describedby="emailHelp" placeholder="Enter email">
@@ -83,7 +110,7 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
+                        <label for="exampleInputPassword1" class='form-text'>Password</label>
                         <div class='input-container'>
                             <i class="fa fa-key icon"></i>
                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
